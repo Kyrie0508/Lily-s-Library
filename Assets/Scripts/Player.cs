becf1d2744b276de90a757bbfa45906ce8ac9eac
Kyrie0508 <ssss3044@gmail.com>
2025-04-05 00:17:19 +0900

    Add plyaer movement and animation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class Plyaer : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = Time.fixedDeltaTime * speed * inputVec.normalized;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        
+        if (inputVec.x != 0 )
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
