bd8cfdd9772576b23149f7433e35a4f6fe32d0e9
Kyrie0508 <ssss3044@gmail.com>
2025-04-19 14:09:36 +0900

    Add Portal

--- Assets/Scripts/MapChange/PortalOutside.cs
+++ Assets/Scripts/MapChange/PortalOutside.cs
@@ -2,12 +2,12 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public class PortalOutside : MonoBehaviour
 {
     private Plyaer player;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("Lounge");
+        SceneManager.LoadScene("Title");
     }
 }
